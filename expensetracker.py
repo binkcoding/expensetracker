9ee50242cfa567dd523185341daa755907e31e23
binkcoding <malachysutton@gmail.com>
2025-06-01 13:09:31 +1000

    Added menu option of adding an expense and writing to csv

--- expensetracker.py
+++ expensetracker.py
@@ -1,1 +1,20 @@
 #Initial Commit
+
+import csv
+from datetime import datetime
+import os
+
+trackerdata = "expensetracker.csv"
+
+menu = input(" Add a new expense.\n View all expenses.\n Update an expense.\n Delete an expense. \n View summary of expenses.\n View summary of expenses for specific month.\n Please choose an option: ").lower()
+
+while menu != "exit":
+    if menu == "add a new expense":
+        amount = float(input("What is the amount? "))
+        category = input("What is the category? ").lower()
+        dateofentry = datetime.now()
+        with open(trackerdata, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([dateofentry, amount, category])
+        print("Expense added")
+
