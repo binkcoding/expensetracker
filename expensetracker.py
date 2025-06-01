2c5ec544ac98b0e42bd2afaefa7ff4149f735e11
binkcoding <malachysutton@gmail.com>
2025-06-01 14:18:27 +1000

    Added menu option of viewing all expenses

--- expensetracker.py
+++ expensetracker.py
@@ -5,16 +5,44 @@ from datetime import datetime
 import os
 
 trackerdata = "expensetracker.csv"
-
-menu = input(" Add a new expense.\n View all expenses.\n Update an expense.\n Delete an expense. \n View summary of expenses.\n View summary of expenses for specific month.\n Please choose an option: ").lower()
+menutext = (
+    "Add a new expense.\n"
+    "View all expenses.\n"
+    "Update an expense.\n"
+    "Delete an expense.\n"
+    "View summary of expenses.\n"
+    "View summary of expenses.\n"
+    "View summary of expenses for specific month.\n"
+    "Please choose an option:  "
+)
+menu = input(menutext).lower()
 
 while menu != "exit":
     if menu == "add a new expense":
         amount = float(input("What is the amount? "))
         category = input("What is the category? ").lower()
-        dateofentry = datetime.now()
+        dateofentry = datetime.now().strftime("%Y-%m-%d %H:%M")
         with open(trackerdata, 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([dateofentry, amount, category])
-        print("Expense added")
+        print("Expense added!")
 
+    elif menu == "view all expenses":
+        with open(trackerdata, 'r', newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                date = row[0]
+                amount = row[1]
+                category = row[2]
+                print(f"Date: {date}, Amount: {amount}, Category: {category}")
+
+    elif menu =="update an expense":
+        with open(trackerdata, 'r', newline='') as file:
+            reader = csv.reader(file)
+            expenses = list(reader)
+
+
+
+
+
+    menu = input(menutext).lower()
